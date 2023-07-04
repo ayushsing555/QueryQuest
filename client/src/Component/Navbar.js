1a2815b97d2561f29c7890e2d4aa56b27ca83d4e
Asifa Khan <khan10asifa@gmail.com>
2023-07-04 11:06:17 +0530

    navbar

--- client/src/Component/Navbar.js
+++ client/src/Component/Navbar.js
@@ -26,15 +26,23 @@ const Navbar = () => {
     }, []);
     
     return (
-        <nav className="bg-transparent dark:bg-red-900   w-full z-20 top-0 left-0 border-b border-red-200 dark:border-red-600">
-            <div className="max-w-screen-2xl flex flex-wrap  items-center justify-between mx-auto p-4">
-                <a href="website" className="flex items-center">
-                    <img src='\image\solutions-community-logo.png' className='object-scale-down h-15 w-20 rounded-lg mt-10' height={10} alt='logo' />
+        <nav className="bg-red-900  w-full  border-b border-black-800 dark:border-slate-600">
+            <div className="max-w-screen-2xl flex flex-wrap  items-center justify-between mx-auto p-2">
+                <a href="website" className="flex items-center -mt-10 ml-2 h-25 w-40">
+                    <img src='\image\Screenshot 2023-07-03 105005.png' className='object-scale-down w-full rounded-xl mt-5' alt='logo'  />
                 </a>
+                {
+                    Details !== null && data[0]!==undefined  ? <>
+                        <b>
+                            <NavLink to="/pricing" className="block text-white-200 border-2  shadow-xl shadow-red-500 rounded md:bg-transparent text-xl  md:text-slate-700 md:p-0 md:dark:text-slate-300 px-2 py-2 mr-2" aria-current="page">{data[0].ticket}</NavLink>
+                        </b>
+                    </> : ""
+                }
+
                 <div className="flex md:order-2">
                     {
                         Details === null ?
-                            <button type="button" className="text-red-900  hover:bg-red-900  hover:bg-white-200 focus:ring-4 focus:outline-none border-red-900 border-4 hover:text-white font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 mt-10 " onClick={changeRoute}>Get started</button> :
+                            <button type="button" className="text-white  hover:bg-white-900  hover:bg-red-200 focus:ring-4 focus:outline-none border-white-900 border-4 hover:text-black font-medium rounded-lg text-sm px-4 py-2 text-center -mt-2 -ml-50 " onClick={changeRoute}>Get started</button> :
                             <UserProfile />
                     }
                     <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
@@ -42,10 +50,10 @@ const Navbar = () => {
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                     </button>
                 </div>
-                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-red-800 md:dark:bg-red-900 dark:border-gray-700">
+                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 -mr-4" id="navbar-sticky">
+                    <ul className="flex flex-col p-2 mt-3 mb-3 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8  md:border-0 md:bg-white dark:bg-red-800 -ml-2 md:dark:bg-red-900 dark:border-gray-700">
                         <li>
-                            <NavLink to="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</NavLink>
+                            <NavLink to="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0  hover:shadow-xl shadow-slate-200 md:dark:text-slate-200" aria-current="page">Home</NavLink>
                         </li>
                         <li>
                             <NavLink to="/about" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</NavLink>
@@ -81,14 +89,7 @@ const Navbar = () => {
                         }
                     </ul>
                 </div>
-                {
-                    Details !== null && data[0]!==undefined  ? <>
-                        <b>
-                            <NavLink to="/pricing" className="block py-2 pl-3 pr-4 text-black border-2 p-2  shadow-xl shadow-red-500 bg-blue-700 rounded md:bg-transparent text-xl  md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">{data[0].ticket}</NavLink>
-                        </b>
-                    </> : ""
-                }
-
+                
             </div>
         </nav>
     );
