999df7575051920d6ab2b644d2e8b69f27540829
Asifa Khan <khan10asifa@gmail.com>
2023-07-04 11:28:03 +0530

    asifa-profile

--- client/src/Component/UserProfile.js
+++ client/src/Component/UserProfile.js
@@ -7,27 +7,35 @@ const UserProfile = () => {
     const Logout = () => {
         localStorage.removeItem("Details");
     };
+    const DeleteAcc=()=>{
+
+    }
  
     return (
            <div class="dropdown">
-            <button class="btn text-white rounded-full  bg-red-900 btn-primary dropdown-toggle" type="button" id="dropdownMenuButton"
+            <button class="btn text-white rounded-full  bg-red-900 ring-4 ring-gray-400 dark:ring-gray-300 hover:bg-red-700 focus:bg-red-900 ml-3  dropdown-toggle" type="button" id="dropdownMenuButton"
                 data-mdb-toggle="dropdown" aria-expanded="false">
                 <img class="w-8 h-8 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500" src="/image/boy1.jpg" alt="Bordered avatar" />
             </button>
-            <ul class="dropdown-menu m-2" aria-labelledby="dropdownMenuButton">
+            <ul class="dropdown-menu text-center py-2 text-white hover:text-black font-bold bg-red-900" aria-labelledby="dropdownMenuButton">
                 <li>
                  <NavLink  to="/profile" class="dropdown-item" href="a">
-                     <button className='btn ml-2'>profile</button>
+                     <button className='py-2'>Profile</button>
                  </NavLink>
                  </li>
                 <li>
                  <NavLink  to="/profile/edit" class="dropdown-item" href="a">
-                     <button className='btn ml-2'>edit profile</button>
+                     <button className='py-2'>Edit Profile</button>
                  </NavLink>
                  </li>
                 <li>
                  <NavLink  to="/signin" class="dropdown-item" href="a">
-                     <button className='btn ml-2' onClick={Logout}>logout</button>
+                     <button className='py-2' onClick={Logout}>Logout</button>
+                 </NavLink>
+                 </li>
+                <li>
+                 <NavLink  to="/signin" class="dropdown-item" href="a">
+                     <button className='py-2 hover:text-red-600' onClick={DeleteAcc}>Delete Account</button>
                  </NavLink>
                  </li>
             </ul>
