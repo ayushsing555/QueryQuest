71178c84c549aae91087191eae8afc7fb0f4f1a4
Asifa Khan <khan10asifa@gmail.com>
2023-07-04 12:28:57 +0530

    home bg

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -5,9 +5,9 @@ const Home = () => {
     <>
       <section class="text-gray-600 body-font">
   <div class="container px-5 py-10 mx-auto">
-    <div class="flex flex-col text-center w-full mb-20">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font  text-gray-900">Ask. Connect. Share Knowledge.</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Your Questions, Answered. Community Driven.</p>
+  <div class="flex flex-col text-center w-full mb-20">
+      <h1 class="sm:text-3xl text-2xl font-bold title-font  text-gray-900">Ask. Connect. Share Knowledge.</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-red-800 font-bold font-serif">Your Questions, Answered. Community Driven.</p>
     </div>
     <div class="flex flex-wrap -m-4">
 
