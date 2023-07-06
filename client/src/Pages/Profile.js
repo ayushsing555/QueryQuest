b19f8f9212db44eaaf94d05eb21f04ae28836ac2
Asifa Khan <khan10asifa@gmail.com>
2023-07-06 09:18:31 +0530

    queryPosted

--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -115,7 +115,7 @@ const Profile =  () => {
                                                     Query Posted:
                                                 </dt>
                                                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                                                    {elem.queryPosted}
+                                                    {elem.TotalQueryPosted}
                                                 </dd>
                                             </div>
                                             <div class="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
