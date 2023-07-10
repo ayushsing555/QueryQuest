dc780d9f548965681dc1b9378f0ea7511b366760
Asifa Khan <khan10asifa@gmail.com>
2023-07-10 17:06:22 +0530

    home pg

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -3,27 +3,33 @@ import { Carousel } from "@material-tailwind/react";
 const Home = () => {
   return (
     <>
-      <section class="bg-orange-100 body-font">
+      <section class=" body-font">
   <div class="container px-5 py-10 mx-auto">
   <div class="flex flex-col text-center w-full mb-20">
       <h1 class="sm:text-3xl text-2xl font-bold title-font  text-gray-900">Ask. Connect. Share Knowledge.</h1>
       <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-red-800 font-bold font-serif">Your Questions, Answered. Community Driven.</p>
     </div>
-    <div class="flex flex-wrap -m-4">
+    
+    <div class="flex flex-wrap ml-80 w-1/2 h-1/3">
 
-    <Carousel transition={{ duration: 2 }} className="rounded-xl">
-      <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" 
+    <Carousel transition={{ duration: 2 }} className="rounded-xl text-black" data-carousel="slide">
+      <img alt="gallery" class="absolute inset-0 w-full" 
         src="/image/query.JPG"
       />
       <img
-        src="/image/query.JPG"
+        src="/image/newQuery.JPG"
         alt="image 2"
-        className="h-100 w-100 object-cover"
+        className=" w-100"
       />
       <img
-        src="/image/query.JPG"
+        src="/image/single Query.JPG"
         alt="image 3"
-        className="h-full w-full object-cover"
+        className=" w-full"
+      />
+      <img
+        src="/image/users.JPG"
+        alt="image 3"
+        className=" w-full"
       />
     </Carousel>
       {/* <div class="lg:w-1/3 sm:w-1/2 p-4">
