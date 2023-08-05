93b254f85871f058a169745c61b72279c6748aa3
Asifa Khan <khan10asifa@gmail.com>
2023-08-05 16:41:35 +0530

    time

--- client/src/Pages/AllQuery.js
+++ client/src/Pages/AllQuery.js
@@ -72,7 +72,7 @@ const AllQuery = () => {
             data-testid="loader"/> </div>
           : <section class="text-black body-font">
             <div className='flex justify-center'>
-              <div>
+              <div className='mt-3'>
                 <input id="bordered-checkbox-1" onClick={dateChange} type="checkbox" value="" name="bordered-checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                 <label for="bordered-checkbox-1" class="w-full py-4 ml-2 text-sm font-medium dark:text-gray-900">Filter By Date</label>
               </div>
