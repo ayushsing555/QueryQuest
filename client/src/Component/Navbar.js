5b469fa189dc64bddcdd5fb525dd0971a3654878
Asifa Khan <khan10asifa@gmail.com>
2023-08-09 17:31:32 +0530

    navbar

--- client/src/Component/Navbar.js
+++ client/src/Component/Navbar.js
@@ -53,33 +53,33 @@ const Navbar = () => {
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 -mr-4" id="navbar-sticky">
                     <ul className="flex flex-col p-2 mt-3 mb-3 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8  md:border-0 md:bg-white dark:bg-red-800 -ml-2 md:dark:bg-red-900 dark:border-gray-700">
                         <li>
-                            <NavLink to="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0  hover:shadow-xl shadow-slate-200 md:dark:text-slate-200" aria-current="page">Home</NavLink>
+                            <NavLink to="/" className="block py-2 -ml-1 px-1 bg-red-900 rounded hover:text-slate-950 hover:font-bold hover:bg-slate-200 md:dark:text-slate-200" aria-current="page">Home</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/about" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</NavLink>
+                            <NavLink to="/about" className="block py-2 -ml-1 px-1 bg-red-900 rounded hover:text-slate-950 hover:font-bold hover:bg-slate-200 md:dark:text-slate-200">About</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/services" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</NavLink>
+                            <NavLink to="/services" className="block py-2 -ml-1 px-1 bg-red-900 rounded hover:text-slate-950 hover:font-bold hover:bg-slate-200 md:dark:text-slate-200">Services</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/contact" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</NavLink>
+                            <NavLink to="/contact" className="block py-2 -ml-1 px-1 bg-red-900 rounded hover:text-slate-950 hover:font-bold hover:bg-slate-200 md:dark:text-slate-200">Contact</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/pricing" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</NavLink>
+                            <NavLink to="/pricing" className="block py-2 -ml-1 px-1 bg-red-900 rounded hover:text-slate-950 hover:font-bold hover:bg-slate-200 md:dark:text-slate-200">Pricing</NavLink>
                         </li>
                         {
                             Details !== null ?
                                 <><li>
-                                    <NavLink to="/newQuery" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Ask Query</NavLink>
+                                    <NavLink to="/newQuery" className="block py-2 -ml-1 px-1 bg-red-900 rounded hover:text-slate-950 hover:font-bold hover:bg-slate-200 md:dark:text-slate-200">Ask Query</NavLink>
                                 </li>
                                     <li>
-                                        <NavLink to="/myQuery" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">My Queries</NavLink>
+                                        <NavLink to="/myQuery" className="block py-2 -ml-1 px-1 bg-red-900 rounded hover:text-slate-950 hover:font-bold hover:bg-slate-200 md:dark:text-slate-200">My Queries</NavLink>
                                     </li>
                                     <li>
-                                        <NavLink to="/allQuery" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">All Queries</NavLink>
+                                        <NavLink to="/allQuery" className="block py-2 -ml-1 px-1 bg-red-900 rounded hover:text-slate-950 hover:font-bold hover:bg-slate-200 md:dark:text-slate-200">All Queries</NavLink>
                                     </li>
                                     <li>
-                                        <NavLink to="/users" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Users</NavLink>
+                                        <NavLink to="/users" className="block py-2 -ml-1 px-1 bg-red-900 rounded hover:text-slate-950 hover:font-bold hover:bg-slate-200 md:dark:text-slate-200">Users</NavLink>
                                     </li>
                                     
                                 </> : ""
