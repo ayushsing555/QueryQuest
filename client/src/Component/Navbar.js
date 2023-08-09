c631a1725dc37385107c7f68d47dd1433491d1d0
Asifa Khan <khan10asifa@gmail.com>
2023-08-09 17:45:44 +0530

    tickets

--- client/src/Component/Navbar.js
+++ client/src/Component/Navbar.js
@@ -1,7 +1,7 @@
 
-import {NavLink, useNavigate} from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import UserProfile from './UserProfile';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 const Navbar = () => {
     const navigator = useNavigate();
     const [data, setData] = useState([]);
@@ -24,17 +24,23 @@ const Navbar = () => {
             getUserDetail(Details);
         }
     }, []);
-    
+
     return (
         <nav className="bg-red-900  w-full  border-b border-black-800 dark:border-slate-600">
             <div className="max-w-screen-2xl flex flex-wrap  items-center justify-between mx-auto p-2">
                 <a href="website" className="flex items-center -mt-10 ml-2 h-25 w-40">
-                    <img src='\image\QueryQuest.png' className='object-scale-down w-full rounded-xl mt-5' alt='logo'  />
+                    <img src='\image\QueryQuest.png' className='object-scale-down w-full rounded-xl mt-5' alt='logo' />
                 </a>
                 {
-                    Details !== null && data[0]!==undefined  ? <>
+                    Details !== null && data[0] !== undefined ? <>
                         <b>
-                            <NavLink to="/pricing" className="block text-white-200 border-2  shadow-xl shadow-red-500 rounded md:bg-transparent text-xl  md:text-slate-700 md:p-0 md:dark:text-slate-300 px-2 py-2 mr-2" aria-current="page">{data[0].ticket}</NavLink>
+                            <NavLink to="/pricing" className="block text-white-200 border-2  shadow-xl shadow-red-500 rounded md:bg-transparent text-xl  md:text-slate-700 md:p-0 md:dark:text-slate-300 px-2 py-2 mr-2" aria-current="page">
+                                {data[0].ticket !== "monthly" ? <h2 class="text-sm tracking-widest title-font mb-3  text-gray-700 font-bold">BASIC</h2> : <h2 class=" text-sm tracking-widest mb-2  text-white font-bold">PRO
+                                    <svg viewBox="0 0 16 16" fill="currentColor" height="10px" className='ml-8 -mt-4 text-yellow-600'>
+                                        <path fillRule="evenodd" d="M8 16A8 8 0 108 0a8 8 0 000 16zm.252-12.932a.478.478 0 00-.682.195l-1.2 2.432-2.684.39a.478.478 0 00-.266.816l1.944 1.892-.46 2.674a.478.478 0 00.694.504L8 10.709l2.4 1.261a.478.478 0 00.694-.504l-.458-2.673L12.578 6.9a.479.479 0 00-.265-.815l-2.685-.39-1.2-2.432a.478.478 0 00-.176-.195z" />
+                                    </svg>
+                                </h2>}
+                            </NavLink>
                         </b>
                     </> : ""
                 }
@@ -81,12 +87,12 @@ const Navbar = () => {
                                     <li>
                                         <NavLink to="/users" className="block py-2 -ml-1 px-1 bg-red-900 rounded hover:text-slate-950 hover:font-bold hover:bg-slate-200 md:dark:text-slate-200">Users</NavLink>
                                     </li>
-                                    
+
                                 </> : ""
                         }
                     </ul>
                 </div>
-                
+
             </div>
         </nav>
     );
