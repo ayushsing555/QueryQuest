524e290abb8a2db71a5f7a17a92850e6a10812b4
Asifa Khan <khan10asifa@gmail.com>
2023-08-17 17:31:42 +0530

    images

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -10,88 +10,50 @@ const Home = () => {
             <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-red-800 font-bold font-serif">Your Questions, Answered. Community Driven.</p>
           </div>
 
-          <div class="flex flex-wrap ml-80 w-1/2 h-1/3">
-
-            <Carousel transition={{ duration: 2 }} className="rounded-xl text-black" data-carousel="slide">
-              <img alt="gallery" class="absolute inset-0 w-full"
-                src="/image/query.JPG"
-              />
-              <img
-                src="/image/newQuery.JPG"
-                alt=" 2"
-                className=" w-100"
-              />
-              <img
-                src="/image/single Query.JPG"
-                alt=" 3"
-                className=" w-full"
-              />
-              <img
-                src="/image/users.JPG"
-                alt=" 3"
-                className=" w-full"
-              />
-            </Carousel>
-            {/* <div class="lg:w-1/3 sm:w-1/2 p-4">
-        <div class="flex relative">
-          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="/image/query.JPG"/>
-          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
-            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
+          <div class="flex flex-wrap ml-80 w-1/2 h-full">
+          <Carousel
+      className="rounded-xl"
+      navigation={({ setActiveIndex, activeIndex, length }) => (
+        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+          {new Array(length).fill("").map((_, i) => (
+            <span
+              key={i}
+              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                activeIndex === i ? "w-8 bg-red-900" : "w-4 bg-red-900/50"
+              }`}
+              onClick={() => setActiveIndex(i)}
+            />
+          ))}
         </div>
-      </div>
-      <div class="lg:w-1/3 sm:w-1/2 p-4">
-        <div class="flex relative">
-          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="/image/query.JPG"/>
-          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
-            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div class="lg:w-1/3 sm:w-1/2 p-4">
-        <div class="flex relative">
-          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="/image/query.JPG"/>
-          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
-            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div class="lg:w-1/3 sm:w-1/2 p-4">
-        <div class="flex relative">
-          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="/image/query.JPG"/>
-          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Neptune</h1>
-            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div class="lg:w-1/3 sm:w-1/2 p-4">
-        <div class="flex relative">
-          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="/image/query.JPG"/>
-          <div class="px-8 py-10 relative z-10 blur-lg w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Holden Caulfield</h1>
-            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div>
-      <div class="lg:w-1/3 sm:w-1/2 p-4">
-        <div class="flex relative">
-          <img alt="gallery" class="absolute inset-0 w-full h-full object-cover object-center" src="/image/query.JPG"/>
-          <div class="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 class="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
-            <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Alper Kamu</h1>
-            <p class="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          </div>
-        </div>
-      </div> */}
+      )}
+    >
+      <img
+        src="/image/query.png"
+        alt="image 1"
+        className="h-full w-1full object-cover"
+      />
+      <img
+        src="/image/singleQuery.png"
+        alt="image 2"
+        className="h-full w-full object-contain"
+      />
+      <img
+        src="/image/users.png"
+        alt="image 3"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="/image/newQuery.png"
+        alt="image 3"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="/image/allQuery.png"
+        alt="image 3"
+        className="h-full w-full object-fit"
+      />
+    </Carousel>
+            
           </div>
         </div>
       </section>
--- client/src/Pages/Pricing.js
+++ client/src/Pages/Pricing.js
@@ -127,14 +127,14 @@ const Pricing = () => {
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                               <path d="M20 6L9 17l-5-5"></path>
                             </svg>
-                          </span>Hexagon neutra unicorn
+                          </span>Able to see analytics of each and every query
                         </p>
                         <p class="flex items-center text-gray-600 mb-4">
                           <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-red-900 text-white rounded-full flex-shrink-0">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
                               <path d="M20 6L9 17l-5-5"></path>
                             </svg>
-                          </span>Mixtape chillwave tumeric
+                          </span>Have a eye on each viewer
                         </p>
                       </div>
                       {
